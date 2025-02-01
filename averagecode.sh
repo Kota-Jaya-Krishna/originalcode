1dbaafec8887c54e4d7e6522a3359f9f82659b9d
Kota-Jaya-Krishna <jayakrishnakota23@gmail.com>
2025-02-01 22:11:45 +0530

    modified

--- averagecode.sh
+++ averagecode.sh
@@ -1,34 +1,64 @@
+# my_function() {
+#   if [ "$1" -gt 10 ]; then
+#     return 0  # Success
+#   else
+#     return 123  # Failure
+#   fi
+# }
+
+# my_function 9
+
+# if [ "$?" -eq 0 ]; then
+#   echo "The function succeeded."
+# else
+#   echo "The function failed."
+# fi
+
+##########or###########
+
+# my_function() {
+#   if [ "$1" -gt 10 ]; then
+#     return 0  # Success
+#     echo "Success"
+#   else
+#     return 123  # Failure
+#   fi
+# }
+
+# my_function 12
+# echo "$?"
+
+# if [ "$?" -eq 0 ]; then
+#   echo "The function succeeded."
+# else
+#   echo "The function failed."
+# fi
+
+
+##########or###########
+
+
 my_function() {
   if [ "$1" -gt 10 ]; then
-    return 0  # Success
+    echo "Given Value is greater than 10"  # Success
   else
     return 123  # Failure
   fi
+  echo "outside of loop"
 }
 
-my_function 4
-
-if [ $? -eq 0 ]; then
-  echo "The function succeeded."
-else
-  echo "The function failed."
-fi
+my_function 12
 
 ##########or###########
 
 my_function() {
   if [ "$1" -gt 10 ]; then
-    return 0  # Success
+    echo "Given Value is greater than 10"  # Success
+    return 0
   else
     return 123  # Failure
   fi
+  echo "outside of loop"
 }
 
-my_function 4
-echo "$?"
-
-if [ $? -eq 0 ]; then
-  echo "The function succeeded."
-else
-  echo "The function failed."
-fi
+my_function 12
